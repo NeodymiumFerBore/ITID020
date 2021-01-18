749037e9ef23ca7a471c54e31dbff25f00719702
NeodymiumFerBore <dev@ndfeb.fr>
2021-01-18 16:49:52 +0100

    Add user creation in init-scripts

--- infra-deployment/04_ansible-deploy-stack/init-scripts/scripts/init-configserver.js
+++ infra-deployment/04_ansible-deploy-stack/init-scripts/scripts/init-configserver.js
@@ -9,4 +9,21 @@ rs.initiate(
          { _id: 2, host : "config03:27017" }
       ]
    }
-)
+);
+db.getSiblingDB("admin").createUser(
+   {
+      user: "admin",
+      pwd: "verylongpassword",
+      roles: [
+         { role: "clusterAdmin", db: "admin" },
+         { role: "userAdmin", db: "admin" }
+      ]
+   },
+   {
+      user: "root",
+      pwd: "verylongpassword",
+      roles: [
+         { role: "root", db: "admin" }
+      ]
+   }
+);
--- infra-deployment/04_ansible-deploy-stack/init-scripts/scripts/init-router.js
+++ infra-deployment/04_ansible-deploy-stack/init-scripts/scripts/init-router.js
@@ -1,3 +1,30 @@
-sh.addShard("shard01/shard01a:27017,shard01b:27017,shard01c:27017")
-sh.addShard("shard02/shard02a:27017,shard02b:27017,shard02c:27017")
-sh.addShard("shard03/shard03a:27017,shard03b:27017,shard03c:27017")
+sh.addShard("shard01/shard01a:27017,shard01b:27017,shard01c:27017");
+sh.addShard("shard02/shard02a:27017,shard02b:27017,shard02c:27017");
+sh.addShard("shard03/shard03a:27017,shard03b:27017,shard03c:27017");
+
+db.getSiblingDB("admin").createUser(
+    {
+       user: "admin",
+       pwd: "verylongpassword",
+       roles: [
+          { role: "clusterAdmin", db: "admin" },
+          { role: "userAdmin", db: "admin" }
+       ]
+    },
+    {
+       user: "root",
+       pwd: "verylongpassword",
+       roles: [
+          { role: "root", db: "admin" }
+       ]
+    }
+ );
+ db.getSiblingDB("itid020").createUser(
+    {
+       user: "itid020",
+       pwd: "SuperPassword",
+       roles: [
+          { role: "readWrite", db: "itid020" }
+       ]
+    }
+ );
