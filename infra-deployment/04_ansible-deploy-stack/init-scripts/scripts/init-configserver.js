5e8c5c794378713b9453d7e79c58731158004b53
NdFeB <dev@ndfeb.fr>
2021-01-18 21:09:38 +0100

    Completely remove user creation part from init-confirserver.sh

--- infra-deployment/04_ansible-deploy-stack/init-scripts/scripts/init-configserver.js
+++ infra-deployment/04_ansible-deploy-stack/init-scripts/scripts/init-configserver.js
@@ -15,28 +15,3 @@ rs.initiate(
 while (!db.isMaster().ismaster) {
 sleep(1000);
 }
-/*
-db.getSiblingDB("admin").createUser(
-   {
-      user: "root",
-      pwd: "verylongpassword",
-      roles: [
-         { role: "root", db: "admin" }
-      ]
-   }
-);
-// Reauth the session
-db.getSiblingDB("admin").auth("root", "verylongpassword");
-
-db.getSiblingDB("admin").createUser(
-   {
-      user: "admin",
-      pwd: "verylongpassword",
-      roles: [
-         { role: "clusterAdmin", db: "admin" },
-         { role: "userAdmin", db: "admin" },
-         { role: "userAdminAnyDatabase", db: "admin" }
-      ]
-   }
-);
-*/
